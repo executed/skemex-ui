a7431c39c2cac3d0ee21630d9ee73d42c5ac2267
Dan Serbyn <dserbyn@agileassets.com>
2019-05-18 19:38:43 +0300

    Change info page url requesting

--- src/app/info-page/infopage.service.ts
+++ src/app/info-page/infopage.service.ts
@@ -9,11 +9,12 @@ import {Observable} from "rxjs";
 @Injectable()
 export class InfoPageService {
 
-  private static infoDocURLRequestURL = `${environment.apiUrl}/infoDocURL`;
+  private static infoDocURLRequestURL = `${environment.apiUrl}/info/infoDocURL`;
 
   constructor(private http: HttpClient) {}
 
   public infoDocURLRequest(): Observable<any> {
+    console.log(InfoPageService.infoDocURLRequestURL);
     return this.http.get(InfoPageService.infoDocURLRequestURL);
   }
 }
