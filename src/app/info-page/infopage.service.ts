3b4a7b1445d257daa9c87bc0fcd9ff8db674c8dd
Dan Serbyn <dserbyn@agileassets.com>
2019-05-20 17:52:17 +0300

    Change info page url requesting

--- src/app/info-page/infopage.service.ts
+++ src/app/info-page/infopage.service.ts
@@ -1,7 +1,5 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
-import {AuthService} from "../auth.service";
-import {LocationStrategy} from "@angular/common";
 import {environment} from "../../environments/environment";
 import {Observable} from "rxjs";
 
